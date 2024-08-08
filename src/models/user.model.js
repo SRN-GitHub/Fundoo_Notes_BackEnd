b352ef96af28513498575145b61c237887f14df8
Shankha-GH <shankharanjannag21@gmail.com>
2024-08-08 17:57:06 +0530

    Registration BackEnd

---
+++ src/models/user.model.js
@@ -0,0 +1,36 @@
+import { required } from '@hapi/joi';
+import { path } from '@hapi/joi/lib/errors';
+import { type } from '@hapi/joi/lib/extend';
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema(
+  {
+    FirstName: {type: String},
+    LastName: {type: String},
+    Email: {type: String, unique: true},
+    Age: {type: Number},
+    Password: {type: String},
+    confirmPassword: {
+      type: String,
+      validate: {
+        validator: function(val) {
+          return val === this.Password;
+        },
+        message: 'Passwords do not match'
+      }
+    }
+  },
+  {
+    timestamps: true
+  }
+);
+
+// userSchema.pre('save', function(next) {
+//   if (this.Password !== this.confirmPassword) {
+//       this.invalidate('confirmPassword', 'Passwords do not match');
+//   }
+//   next();
+// });
+
+
+export default model('User', userSchema);
---
+++ src/validators/user.validator.js
@@ -0,0 +1,21 @@
+import Joi from '@hapi/joi';
+import userModel from '../models/user.model';
+
+export const newUserValidator = (req, res, next) => {
+  const schema = Joi.object({
+    FirstName: Joi.string().min(2).required(),
+    LastName: Joi.string().min(2).required(),
+    Email: Joi.string().email().required(),
+    Age: Joi.number().required(),
+    Password: Joi.string().min(8).required(),
+    ConfirmPassword: Joi.string().min(8).required(),
+  });
+  const { error, value } = schema.validate(req.body);
+  
+  if (error) {
+    next(error);
+  } else {
+    req.validatedBody = value;
+    next();
+  }
+};
